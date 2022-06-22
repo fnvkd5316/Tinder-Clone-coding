6e1d7dcfa723c4b94bed01a7e4eb9999a2673fbd
engin9803 <mne9691@gmail.com>
2022-06-22 22:28:22 +0900

    코드 일부 수정

--- routes/oauth.js
+++ routes/oauth.js
@@ -52,10 +52,9 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  //? 두번 inner 조인해서 나를 팔로우하는 followerid와 내가 팔로우 하는 followingid를 가져와 테이블을 붙인다
-  User.findOne({ where: { id } })
-    .then((user) => done(null, user))
-    .catch((err) => done(err));
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
 });
 
 module.exports = router;
